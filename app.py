0d84743c14aef2c916273cfafd0f575d153b0c5e
red-sat <138263131+red-sat@users.noreply.github.com>
2024-04-11 23:18:06 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import time
+import streamlit as st
+# Create centered main title
+st.title('Chat with MadKudu')
+
+def response_generator(prompt):
+    response = chat_engine.query(prompt)
+    print(type(response))
+    for word in str(response).split():
+        yield word + " "
+        time.sleep(0.05)
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("Ask me anything!"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator(prompt))
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
